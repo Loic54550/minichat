9d97a0142da87a7a41985d0829c36d3b41005013
Alaji <G.loic54550@gmail.com>
2018-11-08 12:09:20 +0100

    mmm

--- Minichat2.php
+++ Minichat2.php
@@ -2,28 +2,44 @@
 <html>
     <head>
         <meta charset="utf-8" />
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="js/bootstrap.js"></script>
         <link rel="stylesheet" href="bootstrap.css" />
         <title>Mini-chat2</title>
     </head>
     <body>
-    <div class="jumbotron">
-        <h1 class="display-4">Bienvenue à Tous</h1>
-        <p class="lead">Les algos sont nos amis pour la vie.</p>
-        <hr class="my-4">
-    </div>
-    <div>
-        <form action="minichat2_post.php" method="post">
-        <p>
-        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
-        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
-        <input type="submit" value="Envoyer" />
-        </p>
-        </form>
-    </div>
-
-
-
-
+        <div class="row">
+            <div class="col-sm-4"></div>
+            <div class="jumbotron">
+                <img src="Sorting_bubblesort_anim.gif">   
+                <h1 class="display-4">Bienvenue à Tous</h1>
+                <p class="lead">Les algos sont nos amis pour la vie.</p>
+                <hr class="my-4">
+            </div>
+        </div>
+        <div>
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Adresse Email</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="ex:linda@gmail.com">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Mot de Passe</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de Passe">
+                </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>    
+        <div>
+            <form action="minichat2_post.php" method="post">
+            <p>
+            <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
+            <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
+            <input type="submit" value="Envoyer" />
+            </p>
+            </form>
+        </div>
 
 
 
@@ -40,19 +56,30 @@ try
 }
 catch(Exception $e)
 {
-        die('Erreur : '.$e->getMessage());
+    die('Erreur : '.$e->getMessage());
 }
 
 // Récupération des 20 derniers messages
-$reponse = $bdd->query('SELECT id, message FROM minichat ORDER BY ID DESC LIMIT 0, 20');
+$reponse = $bdd->prepare('SELECT * 
+                        FROM minichat 
+                        LEFT JOIN utilisateur 
+                            ON utilisateur.id = minichat.utilisateurId; 
+                        ORDER BY ID 
+                        DESC LIMIT 0, 20'
+);
 
 // Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
-while ($donnees = $reponse->fetch())
-{
-    echo '<p><strong>' . htmlspecialchars($donnees['id']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+while ($donnees = $reponse->fetch()) {
+    echo $donnees['pseudo'] . " " . $donnees['message'];
 }
 
-$reponse->closeCursor();
+
+if (isset($_POST['email'])) {
+    $req = $bdd->prepare('INSERT INTO item(email, mdp) VALUES(:email, :mdp)');
+    $req->bindParam(":mdp", $_POST['mdp']);
+    $req->bindParam(":email", $_POST['email']);
+    $req->execute();
+}
 
 ?>
     </body>
